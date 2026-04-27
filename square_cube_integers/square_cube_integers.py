aa3df0445f6afc562434924cecb4b18f7e665cdd
angelalucban07-bit <angelalucban07@gmail.com>
2026-04-27 18:13:54 +0800

    r: simplify class and update file names.

--- square_cube_integers/square_cube_integers.py
+++ square_cube_integers/square_cube_integers.py
@@ -1,15 +1,13 @@
 class NumberProcessor:
-    def __init__(self, filepath="integers.txt"):
-        self.source_filepath = source_filepath
-        self.even_output_filename = "squares.txt"
-        self.odd_output_filename = "cube.txt"
+    def __init__(self):
+        self.source_file = "integers.txt"
+        self.even_output_file = "double.txt"
+        self.odd_output_file = "triple.txt"
 
-    def process_file(self):
-        with open(self.source_filepath, "r") as source_file, \
-open(self.even_output_filename, "w") as even_output_file, \
-open(self.odd_output_filename, "w") as odd_output_file:
+    def output_files(self):
+        with open(self.source_file, "r") as source_file, \
+            open(self.even_output_file, "w") as even_output_file, \
+            open(self.odd_output_file, "w") as odd_output_file:
+
             for line in source_file:
-                numbers_strings = line.split()
-                    for number_strings in numbers_strings:
-
-
+                number_value = int(line.split())
